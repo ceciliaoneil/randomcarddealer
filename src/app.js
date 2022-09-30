37979ddfa15798e4677bac242129eddd144b4243
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-09-30 11:49:01 +0000

    ’cambiosotros’

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let randomNumeros = Math.floor(Math.random() * numeros.length);
+
+  let returnPalotop = (document.getElementById("palotop").innerHTML =
+    palos[randomPalos]);
+
+  let returnNumber = (document.getElementById("number").innerHTML =
+    numeros[randomPalos]);
+
+  let returnPalobottom = (document.getElementById("palobottom").innerHTML =
+    palos[randomPalos]);
+
+  return {
+    returnPalotop,
+    returnNumber,
+    returnPalobottom
+  };
 };
